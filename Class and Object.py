b81afba0358c60120d2fc5ea3f92f7afee7a2205
Rohith Neelagiriyappa <61341128+nrohith55@users.noreply.github.com>
2020-07-15 00:17:32 +0530

    Basic program for non parameterized classes

---
+++ Class and Object.py
@@ -0,0 +1,7 @@
+class zernrays_action:
+    def print_name(self):
+        print("basics of Oops concept")
+
+var=zernrays_action()  #Create the object :Object is real time entity
+
+var.print_name()
